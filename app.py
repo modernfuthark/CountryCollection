c3c99765d26bff50e815bce4ea3f494cb1e3efd7
modernfuthark <timothys1738@gmail.com>
2020-10-27 14:13:09 -0400

    Added basic app

---
+++ app.py
@@ -0,0 +1,15 @@
+#!/usr/bin.python3
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+
+@app.errorhandler(404)
+def notFound():
+    """ 404, Page not found """
+    return jsonify({"error": "Not found"}), 404
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
