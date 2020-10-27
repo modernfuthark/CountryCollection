2988d0df58850605f649474764b5fa7f97964cfb
modernfuthark <timothys1738@gmail.com>
2020-10-27 14:22:05 -0400

    Updates for flask

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!/usr/bin.python3
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,6 +10,10 @@ cors = CORS(app, resources={"r/*": {"Origins": "0.0.0.0"}})
 def notFound():
     """ 404, Page not found """
     return jsonify({"error": "Not found"}), 404
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
