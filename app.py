8a19898bc1dffca48d394f07a926a947fc4a51b5
modernfuthark <timothys1738@gmail.com>
2020-10-27 14:16:39 -0400

    Updated

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 #!/usr/bin.python3
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={"r/*": "0.0.0.0"}})
 
 
 @app.errorhandler(404)
